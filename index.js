fad626ed38f5ae39da85a38a3cd9662d824d8a4a
hrachya.hovsepyan <hrachya.hovsepyan@betconstruct.com>
2017-07-24 12:57:18 +0400

    Setting up routes & adding them into index.js

--- index.js
+++ index.js
@@ -3,6 +3,9 @@ const express = require('express');
 // setting up express app
 const app = express();
 
+// initialize routes
+app.use('/api', /* makes all the routes be preceded by /api  */ require('./routes/api'));
+
 //listen for requests
 app.listen(process.env.port || 4000, function() {
     console.log('Now listening for requests');
---
+++ routes/api.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const router = express.Router();
+
+// Get a list of ninjas from the DB
+router.get('/ninjas', function(req, res) {
+    res.send({type: 'GET'});
+});
+
+// Add a new ninja to the DB
+router.post('/ninjas', function(req, res) {
+    res.send({type: 'POST'});
+});
+
+// Update a ninja in the DB
+router.put('/ninjas/:id' /* id is a parameter/variable, that's why we need ':' */, function(req, res) {
+    res.send({type: 'PUT'});
+});
+
+// Delete a ninja from the DB
+router.delete('/ninjas/:id', function(req, res) {
+    res.send({type: 'DELETE'});
+});
+
+module.exports = router;
