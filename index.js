dd4819ef30682441fa2d72e15480e6057ac1495f
hrachya.hovsepyan <hrachya.hovsepyan@betconstruct.com>
2017-07-25 12:39:50 +0400

    Added error handling

--- index.js
+++ index.js
@@ -16,6 +16,12 @@ app.use(bodyParser.json());
 // initialize routes
 app.use('/api', /* makes all the routes be preceded by /api  */ require('./routes/api'));
 
+// Error handling middleware
+app.use(function(err, req, res, next) {
+    //console.log(err);
+    res.status(422).send({error: err._message});
+});
+
 //listen for requests
 app.listen(process.env.port || 4000, function() {
     console.log('Now listening for requests');
--- routes/api.js
+++ routes/api.js
@@ -3,26 +3,24 @@ const router = express.Router();
 const Ninja = require('../models/ninja');
 
 // Get a list of ninjas from the DB
-router.get('/ninjas', function(req, res) {
+router.get('/ninjas', function(req, res, next) {
     res.send({type: 'GET'});
 });
 
 // Add a new ninja to the DB
-router.post('/ninjas', function(req, res) {
+router.post('/ninjas', function(req, res, next) {
     Ninja.create(req.body).then(function(result) {
         res.send(result);
-    }).catch(function(e) {
-        res.send(e.message);
-    });
+    }).catch(next);
 });
 
 // Update a ninja in the DB
-router.put('/ninjas/:id' /* id is a parameter/variable, that's why we need ':' */, function(req, res) {
+router.put('/ninjas/:id' /* id is a parameter/variable, that's why we need ':' */, function(req, res, next) {
     res.send({type: 'PUT'});
 });
 
 // Delete a ninja from the DB
-router.delete('/ninjas/:id', function(req, res) {
+router.delete('/ninjas/:id', function(req, res, next) {
     res.send({type: 'DELETE'});
 });
 
