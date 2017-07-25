526cf945092eecaf8848c9e227fbea57ce3f4e2e
hrachya.hovsepyan <hrachya.hovsepyan@betconstruct.com>
2017-07-25 15:55:31 +0400

    Added index.html & css, plus added express.static

--- index.js
+++ index.js
@@ -11,6 +11,8 @@ mongoose.connect('mongodb://localhost/ninjago', {
 });
 mongoose.Promise = global.Promise;
 
+app.use(express.static('public'));
+
 app.use(bodyParser.json());
 
 // initialize routes
