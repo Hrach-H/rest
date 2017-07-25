c853895ae724fe6011a942b196ac5d7a622b5ba6
hrachya.hovsepyan <hrachya.hovsepyan@betconstruct.com>
2017-07-25 14:08:47 +0400

    Added geolocation schema

--- models/ninja.js
+++ models/ninja.js
@@ -1,6 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+
+// creating geolocation Schema (taken from GeoJSON 'geometry')
+const GeoShema = new Schema({
+    type: {
+        type: String,
+        default: "Point"
+    },
+    coordinates: {
+        type: [Number],
+        index: "2dsphere"
+    }
+});
+
 // creating ninja Schema & model
 const NinjaSchema = new Schema({
     name: {
@@ -13,8 +26,8 @@ const NinjaSchema = new Schema({
     available: {
         type: Boolean,
         default: false
-    }
-    // add in geo location
+    },
+    geometry: GeoShema
 });
 
 const Ninja = mongoose.model('ninja', NinjaSchema);
